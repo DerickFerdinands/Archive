b2a5a04dd56b7ee3ffb711de2dce5e8e6be01640
DerickFerdinands <derickandrewferdinands@gmail.com>
2023-07-17 19:27:57 +0530

    Hero Page In Progress

--- Archive/front-end/src/components/home/HomePage.jsx
+++ Archive/front-end/src/components/home/HomePage.jsx
@@ -1,5 +1,9 @@
-export const HopePage = ()=>{
+import {Nav} from "./nav/Nav";
+import {Hero} from "./hero/Hero";
 
+export const HopePage = () => {
     return <>
+        <Nav/>
+        <Hero/>
     </>
 }
