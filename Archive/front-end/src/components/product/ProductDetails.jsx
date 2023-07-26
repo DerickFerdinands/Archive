1b694513c170035f1883c7f4dea2f4900b188506
DerickFerdinands <derickandrewferdinands@gmail.com>
2023-07-26 08:44:37 +0530

    Item Details Options Configured

--- Archive/front-end/src/components/product/ProductDetails.jsx
+++ Archive/front-end/src/components/product/ProductDetails.jsx
@@ -4,58 +4,6 @@ import {RadioGroup} from '@headlessui/react'
 import axios from "axios";
 import {useParams} from "react-router-dom";
 
-/*const product = {
-    name: 'Basic Tee 6-Pack',
-    price: '$192',
-    href: '#',
-    breadcrumbs: [
-        {id: 1, name: 'Men', href: '#'},
-        {id: 2, name: 'Clothing', href: '#'},
-    ],
-    images: [
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
-            alt: 'Two each of gray, white, and black shirts laying flat.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
-            alt: 'Model wearing plain black basic tee.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
-            alt: 'Model wearing plain gray basic tee.',
-        },
-        {
-            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
-            alt: 'Model wearing plain white basic tee.',
-        },
-    ],
-    colors: [
-        {name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400'},
-        {name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400'},
-        {name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900'},
-    ],
-    sizes: [
-        {name: 'XXS', inStock: false},
-        {name: 'XS', inStock: true},
-        {name: 'S', inStock: true},
-        {name: 'M', inStock: true},
-        {name: 'L', inStock: true},
-        {name: 'XL', inStock: true},
-        {name: '2XL', inStock: true},
-        {name: '3XL', inStock: true},
-    ],
-    description:
-        'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-    highlights: [
-        'Hand cut and sewn locally',
-        'Dyed with our proprietary colors',
-        'Pre-washed & pre-shrunk',
-        'Ultra-soft 100% cotton',
-    ],
-    details:
-        'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-}*/
 const reviews = {href: '#', average: 4, totalCount: 117}
 
 function classNames(...classes) {
@@ -66,23 +14,22 @@ export const ProductDetails = () => {
 
     let {code} = useParams();
 
-    const [selectedColor, setSelectedColor] = useState('')
-    const [selectedSize, setSelectedSize] = useState('')
+    const [selectedSize, setSelectedSize] = useState('XL')
 
-    const [product, setProduct] = useState({imageUrls:[],options:[]});
+    const [product, setProduct] = useState({imageUrls: [], options: []});
 
-    useEffect(()=>{
+    useEffect(() => {
         axios({
-            method:'get',
-            url:`http://localhost:3001/api/v1/product/${code}`
-        }).then((response)=>{
+            method: 'get',
+            url: `http://localhost:3001/api/v1/product/${code}`
+        }).then((response) => {
             console.log({...response.data.data})
             setProduct({...response.data.data})
         })
-    },[])
+    }, [])
 
     return <>
-       <div className="bg-white">
+        <div className="bg-white">
             <div className="pt-6">
                 <nav aria-label="Breadcrumb">
                     <ol role="list"
@@ -167,27 +114,27 @@ export const ProductDetails = () => {
                         <form className="mt-10">
 
 
-
                             <div className="mt-10">
                                 <div className="flex items-center justify-between">
                                     <h3 className="text-sm font-medium text-gray-900">Size</h3>
 
                                 </div>
 
-                                <RadioGroup value={selectedSize} onChange={setSelectedSize} className="mt-4">
+                                <RadioGroup value={selectedSize} onChange={(change)=>{setSelectedSize(change.optionName)}} className="mt-4">
                                     <RadioGroup.Label className="sr-only">Choose a size</RadioGroup.Label>
                                     <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
                                         {product.options.map((size) => (
                                             <RadioGroup.Option
                                                 key={size.optionName}
                                                 value={size}
-                                                disabled={!size.optionQty<=0}
+                                                disabled={size.optionQty <= 0}
+                                                onCl
                                                 className={({active}) =>
                                                     classNames(
-                                                        size.optionQty>0
+                                                        size.optionQty > 0
                                                             ? 'cursor-pointer bg-white text-gray-900 shadow-sm'
                                                             : 'cursor-not-allowed bg-gray-50 text-gray-200',
-                                                        size.optionQty>0 ? 'ring-2 ring-indigo-500' : '',
+                                                        selectedSize === size.optionName ? 'ring-2 ring-indigo-500' : '',
                                                         'group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6'
                                                     )
                                                 }
@@ -195,10 +142,10 @@ export const ProductDetails = () => {
                                                 {({active, checked}) => (
                                                     <>
                                                         <RadioGroup.Label as="span">{size.optionName}</RadioGroup.Label>
-                                                        {size.optionQty>0 ? (
+                                                        {size.optionQty > 0 ? (
                                                             <span
                                                                 className={classNames(
-                                                                    size.optionQty>0 ? 'border' : 'border-2',
+                                                                    (size.optionQty > 0) ? 'border' : 'border-2',
                                                                     checked ? 'border-indigo-500' : 'border-transparent',
                                                                     'pointer-events-none absolute -inset-px rounded-md'
                                                                 )}
@@ -209,15 +156,16 @@ export const ProductDetails = () => {
                                                                 aria-hidden="true"
                                                                 className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
                                                             >
-                                <svg
-                                    className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                    viewBox="0 0 100 100"
-                                    preserveAspectRatio="none"
-                                    stroke="currentColor"
-                                >
-                                  <line x1={0} y1={100} x2={100} y2={0} vectorEffect="non-scaling-stroke"/>
-                                </svg>
-                              </span>
+                                                                  <svg
+                                                                      className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
+                                                                      viewBox="0 0 100 100"
+                                                                      preserveAspectRatio="none"
+                                                                      stroke="currentColor"
+                                                                  >
+                                                                 <line x1={0} y1={100} x2={100} y2={0}
+                                                                       vectorEffect="non-scaling-stroke"/>
+                                                               </svg>
+                                                              </span>
                                                         )}
                                                     </>
                                                 )}
@@ -238,7 +186,7 @@ export const ProductDetails = () => {
 
                     <div
                         className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-                         Description and details
+                        Description and details
                         <div>
                             <h3 className="sr-only">Description</h3>
 
